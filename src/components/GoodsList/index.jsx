dc1542e3730016818a7ca68d07a776d67ab6451c
n1ckn0ck3r <bondarenko.kolya.a@gmail.com>
2022-07-05 15:14:46 +0300

    Update index.jsx

--- src/components/GoodsList/index.jsx
+++ src/components/GoodsList/index.jsx
@@ -6,45 +6,45 @@ import styles from "./GoodsList.module.css";
 import GoodCard from "../GoodCard";
 
 const GoodsList = () => {
-	const { good } = useContext(Context);
-	const navigate = useNavigate();
+  const { good } = useContext(Context);
+  const navigate = useNavigate();
 
-	useEffect(() => {
-		good.setSelectedCategories(
-			JSON.parse(localStorage.getItem("selected-categories")) || good.categories
-		);
-		good.setSelectedMarkets(
-			JSON.parse(localStorage.getItem("selected-markets")) || good.markets
-		);
-	}, [good]);
+  useEffect(() => {
+    good.setSelectedCategories(
+      JSON.parse(localStorage.getItem("selected-categories")) || good.categories
+    );
+    good.setSelectedMarkets(
+      JSON.parse(localStorage.getItem("selected-markets")) || good.markets
+    );
+  }, [good]);
 
-	const selectGood = (item) => {
-		good.setSelectedGood(item);
-		localStorage.setItem("selected-good", JSON.stringify(good.selectedGood));
-		navigate(`/good/${item.id}`);
-	};
+  const selectGood = (item) => {
+    good.setSelectedGood(item);
+    localStorage.setItem("selected-good", JSON.stringify(good.selectedGood));
+    navigate(`/good/${item.id}`);
+  };
 
-	return (
-		<div className={styles.goodsList}>
-			{good.goods
-				.filter(
-					(item) =>
-						good.selectedCategories.includes(item.category) &&
-						good.selectedMarkets.includes(item.market)
-				)
-				.map((goodItem) => (
-					<GoodCard
-						key={goodItem.id}
-						title={goodItem.title}
-						price={goodItem.price}
-						img={goodItem.image}
-						category={goodItem.category}
-						market={goodItem.market}
-						addToCart={() => selectGood(goodItem)}
-					/>
-				))}
-		</div>
-	);
+  return (
+    <div className={styles.goodsList}>
+      {good.goods
+        .filter(
+          (item) =>
+            good.selectedCategories.includes(item.category) &&
+            good.selectedMarkets.includes(item.market)
+        )
+        .map((goodItem) => (
+          <GoodCard
+            key={goodItem.id}
+            title={goodItem.title}
+            price={goodItem.price}
+            img={goodItem.image}
+            category={goodItem.category}
+            market={goodItem.market}
+            addToCart={() => selectGood(goodItem)}
+          />
+        ))}
+    </div>
+  );
 };
 
 export default observer(GoodsList);
